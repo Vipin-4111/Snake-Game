b55709d3551a7ddfc1997e1c7371349e61a8c933
Vipin Kumar <151990039+Vipin-4111@users.noreply.github.com>
2025-04-18 21:16:50 +0530

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,12 @@
+package My_Game;
+
+//*****************************************
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
+//*****************************************
+
